917ad431cb96654fa1092ef9c319f7196e375028
Dennis Minn <minndennis@gmail.com>
2024-05-23 04:04:35 -0400

    added persistent storage for FAQ, modified addQA behavior, minor bug fixes

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
+import requests
 from dotenv import load_dotenv
-from twitchio.ext import commands
+# from twitchio.ext import commands
+from TwitchIO.twitchio.ext import commands
 from stream_guard import StreamGuardBot
 
 
@@ -14,15 +16,27 @@ config = {
     'channel': os.environ['INITIAL_CHANNEL']
 }
 
+
 class Bot(commands.Bot):
     def __init__(self):
-        super().__init__(token=config['access_token'], prefix='!', initial_channels = [config['channel']])
-        self.channels = {config['channel']: StreamGuardBot(config['channel'])}
+        with open(f"channels/{config['channel']}.jsonl", 'a') as channel_file:
+            pass
+
+        os.makedirs('channel', exist_ok=True)
+        initial_channels = [
+            os.path.splitext(channel_file)[0] for channel_file
+            in os.listdir('channels')
+        ]
+
+        super().__init__(token=config['access_token'], prefix='!', initial_channels = initial_channels)
+        self.channels = {}
+        for channel in initial_channels:
+            self.channels[channel] = StreamGuardBot(channel)
 
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
         print(f'User id is | {self.user_id}')
-
+        
     async def event_message(self, message):
         if message.echo:
             return
@@ -30,15 +44,20 @@ class Bot(commands.Bot):
         if message.content.startswith('!'):
             await self.handle_commands(message)
         else:
-            message.content = '!ask {question}'.format(question=message.content)
+            message.content = '!_ask {question}'.format(question=message.content)
             await self.handle_commands(message)
 
     @commands.command(name='guard')
-    async def add_channel(self, context: commands.Context):
-        channel = context.author.name
-        await context.send(f'{channel} is now guarded!')
-        await self.event_join(channel, context.author)
+    async def add_channel(self, context: commands.Context, channel: str):
+        if context.author.name != 'dennosity':
+            return
 
+        if channel in self.channels:
+            await context.send(f'{channel} is already guarded!')
+            return
+
+        await context.send(f'{channel} is now guarded!')
+        await self.join_channels([channel])
         # Stream Guard Bot Section
         self.channels[channel] = StreamGuardBot(channel)
         
@@ -52,9 +71,10 @@ class Bot(commands.Bot):
             return
         
         await context.send(f"Stream Guard Bot has left {channel}'s chat")
-        await self.event_part(context.get_user(channel))
+        await self.part_channels([channel])
         
         del self.channels[channel]
+        os.remove(f'channels/{channel}.jsonl')
 
     @commands.command(name='addQA')
     async def add_qa(self, context: commands.Context, question: str, answer: str):
@@ -64,6 +84,7 @@ class Bot(commands.Bot):
         channel = context.channel.name
         stream_guard_bot = self.channels[channel]
         stream_guard_bot.add_qa(question, answer)
+        await context.send(f'Added {question} -> {answer} ')
 
     @commands.command(name='removeQA')
     async def remove_qa(self, context: commands.Context, index: int):
@@ -81,11 +102,22 @@ class Bot(commands.Bot):
         faq = stream_guard_bot.list_faq()
         await context.send(faq)
 
+    @commands.command(name='_ask')
+    async def _ask(self, context: commands.Context, *, question: str):
+        channel = context.channel.name
+        stream_guard_bot = self.channels[channel]
+        response = stream_guard_bot._respond(question)
+        
+        if response == '':
+            return
+        
+        await context.reply(response)
+
     @commands.command(name='ask')
     async def ask(self, context: commands.Context, *, question: str):
         channel = context.channel.name
         stream_guard_bot = self.channels[channel]
-        response = stream_guard_bot.respond([question])
+        response = stream_guard_bot.respond(question)
         
         if response == '':
             return
--- stream_guard.py
+++ stream_guard.py
@@ -1,55 +1,62 @@
 import os
+import json
 import numpy as np
 import faiss
-from dotenv import load_dotenv
 from openai import OpenAI
-from sentence_transformers import SentenceTransformer
 
 from typing import List
 
-load_dotenv(override=True)
-
-system_prompt = (
-    "Users are communicating with {channel}, not the AI "
-    "To answer, refer to the << FAQ >>."
-    "Keep your responses concise and respond in the 3rd person. "
-    "If the answer is not provided in the << FAQ >>, respond with 'I do not know'. "
-    "Do not make up your response or use external information.\n"
-    "<< FAQ >>\n"
-    "{documents}\n"
-)
-
-open_ai_secret = os.environ['OPENAI_API_KEY']
-
-embedding_model_id = 'sentence-transformers/all-mpnet-base-v2'
-embedding_model = SentenceTransformer(embedding_model_id)
-
 client = OpenAI()
+embedding_size = 1536
+embedding_model_id = 'text-embedding-3-small'
+generation_model_id = 'gpt-3.5-turbo'
 
 class StreamGuardBot:
     def __init__(self, channel: str):
         self.channel = channel
-        self.embedding_model = embedding_model
         self.faq = []
         self.response_threshold = 1
-
-        embedding_size = embedding_model.get_sentence_embedding_dimension()
         self.vector_database = faiss.IndexFlatL2(embedding_size)
 
-    def add_qa(self, question: str, answer: str):
-        document = {'question': question, 'answer': answer}
-        self.faq.append(document)
+        faq = []
+        if os.path.exists(f'channels/{self.channel}.jsonl'):
+            with open(f'channels/{self.channel}.jsonl') as channel_file:
+                faq = [json.loads(line) for line in channel_file]
 
-        document_embedding = self.embedding_model.encode([document])
-        self.vector_database.add(document_embedding)
+            # Remove the old FAQ, otherwise there will be duplicates
+            os.remove(f'channels/{self.channel}.jsonl')
+
+        # Re-add the FAQ
+        for qa in faq:
+            self.add_qa(qa['question'], qa['answer'])
+                    
+    def add_qa(self, question: str, answer: str):
+        qa = {'question': question, 'answer': answer}
+        self.faq.append(qa)
+
+        response = client.embeddings.create(
+            input=[question],
+            model=embedding_model_id
+        )
+
+        qa_embedding = np.array([response.data[0].embedding])
+        self.vector_database.add(qa_embedding)
+        with open(f'channels/{self.channel}.jsonl', 'a') as channel_file:
+            qa_json = json.dumps(qa)
+            channel_file.write(qa_json + '\n')
 
     def remove_qa(self, index: int):
-        # TODO allow for multiple indices
-
         index = index - 1
-        self.faq.pop(index)
+        qa = self.faq.pop(index)
+
         # FAISS expects a 2-d np.array
         self.vector_database.remove_ids(np.array([index]))
+        with open(f'channels/{self.channel}.jsonl', 'w') as channel_file:
+            for qa in self.faq:
+                qa_json = json.dumps(qa)
+                channel_file.write(qa_json + '\n')
+
+        return f"{qa['question']} -> {qa['answer']}"
 
     def list_faq(self):
         faq = [
@@ -60,36 +67,88 @@ class StreamGuardBot:
         faq = ' | '.join(faq)
         return faq
     
-    def respond(self, queries: List[str]):
-        # TODO batch responses
-
+    def respond(self, question: str):
         # Ignores query if empty database is empty
         if self.vector_database.ntotal < 1:
             return ''
         
-        query_embeddings = self.embedding_model.encode(queries)
-        distances, indices = self.vector_database.search(query_embeddings, 1)
-        documents = [str(self.faq[index[0]]) for index in indices]
+        system_prompt = (
+            "Users are communicating with {channel}, not the AI "
+            "To answer, refer to the << FAQ >>."
+            "Keep your responses concise and respond in the 3rd person. "
+            "Do not make up your response or use external information.\n"
+            "<< FAQ >>\n"
+            "{faq}\n"
+        )
 
-        # Ignores query if retrieved document is unrelated
-        if distances[0] > self.response_threshold:
-            return ''
+        embedding_api_response = client.embeddings.create(
+            input=[question],
+            model=embedding_model_id
+        )
+        question_embedding = np.array([embedding_api_response.data[0].embedding])
+        
+        distance, faq_index = self.vector_database.search(question_embedding, 1)
+        distance, faq_index = distance.item(), faq_index.item()
 
         bot_prompt = system_prompt.format(
             channel=self.channel,
-            documents='\n'.join(documents)
+            faq=str(self.faq[faq_index])
         )
         
-        response = client.chat.completions.create(
-            model='gpt-3.5-turbo',
+        generation_api_response = client.chat.completions.create(
+            model=generation_model_id,
             messages=[
                 {'role': 'system', 'content': bot_prompt},
-                {'role': 'user', 'content': queries[0]}
+                {'role': 'user', 'content': question}
             ],
             max_tokens=75,
             temperature=0
         )
 
-        message = response.choices[0].message.content
+        message = generation_api_response.choices[0].message.content
+        return message
+    
+    def _respond(self, question: str):
+        # Ignores query if empty database is empty
+        if self.vector_database.ntotal < 1:
+            return ''
+
+        system_prompt = (
+            "Users are communicating with {channel}, not the AI "
+            "To answer, refer to the << FAQ >>."
+            "Keep your responses concise and respond in the 3rd person. "
+            "If the answer is not provided in the << FAQ >>, respond with 'I do not know'. "
+            "Do not make up your response or use external information.\n"
+            "<< FAQ >>\n"
+            "{faq}\n"
+        )
         
-        return message if message != 'I do not know' else ''
+        embedding_api_response = client.embeddings.create(
+            input=[question],
+            model=embedding_model_id
+        )
+        question_embedding = np.array([embedding_api_response.data[0].embedding])
+        
+        distance, faq_index = self.vector_database.search(question_embedding, 1)
+        distance, faq_index = distance.item(), faq_index.item()
+
+        if distance > self.response_threshold:
+            return ''
+
+        bot_prompt = system_prompt.format(
+            channel=self.channel,
+            faq=str(self.faq[faq_index])
+        )
+        
+        generation_api_response = client.chat.completions.create(
+            model=generation_model_id,
+            messages=[
+                {'role': 'system', 'content': bot_prompt},
+                {'role': 'user', 'content': question}
+            ],
+            max_tokens=75,
+            temperature=0
+        )
+
+        message = generation_api_response.choices[0].message.content
+        return message if message != 'I do not know.' else ''
